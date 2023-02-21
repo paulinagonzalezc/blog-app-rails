dc71634f1cf00c6916fc3cfdb385ef0306ba1e8c
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-02-21 17:38:24 -0500

    Add user ref to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_221201) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_223547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_221201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -46,4 +48,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_221201) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
 end
