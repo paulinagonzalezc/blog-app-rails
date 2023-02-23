99c6a5227837fb5fba4410d2693360a6a4627fec
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-02-22 23:47:08 -0500

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "author_id"
-  belongs_to :post, class_name: "Post", foreign_key: "post_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   after_create :update_post_comments_counter
   after_destroy :update_post_comments_counter
