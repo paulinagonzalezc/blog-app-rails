ac9fba640ed3649206c2e8ffa99bea60f1405887
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-02-24 00:19:36 -0500

    Set counters to 0 and add tests for Post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before :each do
+    @user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Student')
+    @post = Post.create(author: @user, title: 'My post', text: 'Hello!')
+  end
+
+  it 'checks title presence' do
+    @post.title = nil
+    expect(@post).to_not be_valid
+  end
+
+  it 'checks validation for comments counter' do
+    @post.comments_counter = nil
+    expect(@post).to_not be_valid
+  end
+
+  it 'checks if author id is present' do
+    @post.author_id = nil
+    expect(@post).to_not be_valid
+  end
+
+  it 'checks if comments counter is an integer' do
+    @post.comments_counter = 'hello'
+    expect(@post).to_not be_valid
+  end
+
+  it 'checks if likes counter is an integer' do
+    @post.likes_counter = 'hello'
+    expect(@post).to_not be_valid
+  end
+
+  describe 'Check methods in post' do
+    before do
+      @user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Student')
+      @post1 = Post.create(author: @user, title: 'My post', text: 'Hello!')
+      6.times do
+        Comment.create(post: @post1, author: @user, text: 'Hi Tom!' )
+      end
+    end
+
+    it 'should have the five recent comments method' do
+      expect(@post).to respond_to(:recent_comments)
+    end
+
+    it 'should return an empty array' do
+      expect(@post1.recent_comments.count).to eql(5)
+    end
+  end
+end
+
+
+ # describe 'Check methods in post' do
+  #   before do
+  #     @user = User.new(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Student')
+  #     @post1 = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
+  #     5.times do
+  #       Comment.create(post: @post1, author: @user, text: 'Hi Paulina!')
+  #     end
+  #   end
+
+  #   it 'should have the five recent comments method' do
+  #     expect(@post).to respond_to(:recent_comments)
+  #   end
+
+  #   it 'should return an empty array' do
+  #     expect(@post1.recent_comments.count).to eql(0)
+  #   end
+  # end
