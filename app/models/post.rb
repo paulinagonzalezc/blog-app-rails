0b6e05f1b7dbfcff33fcdce5da0b516828aa34a3
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-02-23 20:51:04 -0500

    Add validations for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,9 @@ class Post < ApplicationRecord
   after_create :update_post_counter
   after_destroy :update_post_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
