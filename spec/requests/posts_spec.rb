a5d77b1037a63f98e0fb0a8575dcfbd264cae01b
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-02-28 15:04:45 -0500

    Add tests for user controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,43 +4,43 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /users/:user_id/posts' do
     # If response status was correct.
     it 'returns a success response' do
-      user = User.create(name: 'Paulina', photo: 'photo', bio: 'Engineer')
+      user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer')
       get "/users/#{user.id}/posts"
       expect(response).to have_http_status(:success)
     end
     # If a correct template was rendered.
     it 'renders the index template' do
-      user = User.create(name: 'Paulina', photo: 'photo', bio: 'Engineer')
+      user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer')
       get "/users/#{user.id}/posts"
       expect(response).to render_template(:index)
     end
     # If the response body includes correct placeholder text.
     it 'includes the correct placeholder text in the response body' do
-      user = User.create(name: 'Paulina', photo: 'photo', bio: 'Engineer')
+      user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer')
       post = Post.create(title: 'My First Post', text: 'Hello World', author: user)
       get "/users/#{user.id}/posts"
       expect(response.body).to include(post.title)
     end
   end
 
-  describe 'GET /users/:user_id/posts/:id' do
+  describe 'GET #show' do
     # If response status was correct.
     it 'returns a success response' do
-      user = User.create(name: 'Paulina', photo: 'photo', bio: 'Engineer')
+      user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer')
       post = Post.create(title: 'My First Post', text: 'Hello World', author: user)
       get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to have_http_status(:success)
     end
     # If a correct template was rendered.
     it 'renders the show template' do
-      user = User.create(name: 'Paulina', photo: 'photo', bio: 'Engineer')
+      user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer')
       post = Post.create(title: 'My First Post', text: 'Hello World', author: user)
       get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to render_template(:show)
     end
     # If the response body includes correct placeholder text.
     it 'includes the correct placeholder text in the response body' do
-      user = User.create(name: 'Paulina', photo: 'photo', bio: 'Engineer')
+      user = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer')
       post = Post.create(title: 'My First Post', text: 'Hello World', author: user)
       get "/users/#{user.id}/posts/#{post.id}"
       expect(response.body).to include(post.title)
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe UsersController, type: :request do
+  describe 'GET #index' do
+    # If response status was correct.
+    it 'returns http success' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+    # If a correct template was rendered.
+    it 'renders the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+    # If the response body includes correct placeholder text.
+    it 'displays the correct users' do
+      user1 = User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer')
+      user2 = User.create(name: 'Julian', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Astronaut')
+      get users_path
+      expect(response.body).to include(user1.name)
+      expect(response.body).to include(user2.name)
+    end
+  end
+
+  describe 'GET #show' do
+    let(:user) { User.create(name: 'Paulina', photo: 'https://unsplash.com/es/fotos/vuBaykPW1Dk', bio: 'Engineer') }
+    # If response status was correct.
+    it 'returns http success' do
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+    end
+    # If a correct template was rendered.
+    it 'renders the show template' do
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+    # If the response body includes correct placeholder text.
+    it 'displays the correct user' do
+      get user_path(user)
+      expect(response.body).to include(user.name)
+      expect(response.body).to include(user.photo)
+      expect(response.body).to include(user.bio)
+    end
   end
 end
