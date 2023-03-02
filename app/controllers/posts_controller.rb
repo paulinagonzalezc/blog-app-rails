78745c49fb98e0000cd7206010f416ddbd09fbed
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-03-02 17:20:03 -0500

    Add form and functionality for new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,24 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find_by!(id: params[:id], author_id: params[:user_id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_post_path(@post.author, @post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:new_post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show], param: :id
+    resources :posts, only: [:index, :show, :new, :create], param: :id
   end 
 
   root 'users#index'
