bf250c796f2bf2f29cc8caf9c1e6df9ce6a6b5ef
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-03-07 19:23:58 -0500

    Make migration for user devise table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+userOne = User.create(name: "Paulina", photo: "https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwZG9nfGVufDB8fDB8fA%3D%3D&w=1000&q=80", bio: "paulina", email: "paulinagzzc@gmail.com", password: "12345678")
+userTwo = User.create(name: "Paulina", photo: "https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwZG9nfGVufDB8fDB8fA%3D%3D&w=1000&q=80", bio: "paulina", email: "paulinagzzc@gmail.com", password: "12345678")
+userThree = User.create(name: "Paulina", photo: "https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwZG9nfGVufDB8fDB8fA%3D%3D&w=1000&q=80", bio: "paulina", email: "paulinagzzc@gmail.com", password: "12345678")
+userFour = User.create(name: "Paulina", photo: "https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwZG9nfGVufDB8fDB8fA%3D%3D&w=1000&q=80", bio: "paulina", email: "paulinagzzc@gmail.com", password: "12345678")
+userFive = User.create(name: "Paulina", photo: "https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwZG9nfGVufDB8fDB8fA%3D%3D&w=1000&q=80", bio: "paulina", email: "paulinagzzc@gmail.com", password: "12345678")
