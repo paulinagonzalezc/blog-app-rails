ebc771d35866e164175ff8b756d34097237a81b7
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-03-08 13:40:16 -0500

    Add privilege to user in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id', dependent: :destroy
 
   after_create :update_post_counter
   after_destroy :update_post_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
-  has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, class_name: 'Comment', foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
@@ -14,5 +14,15 @@ class User < ApplicationRecord
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  # User::Roles
+  # The available roles
+  ROLES = %w[admin].freeze
+
+  ROLES.each do |exact_role|
+    define_method "#{exact_role}?" do
+      role == exact_role
+    end
   end
 end
